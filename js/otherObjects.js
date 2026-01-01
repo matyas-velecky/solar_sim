48ec464cd10be446753543f2fa28570bf4de485c
matyas-velecky <matyas.velecky@gmail.com>
2026-01-01 21:49:04 +0100

    Change asteroid material to MeshBasicMaterial

--- js/otherObjects.js
+++ js/otherObjects.js
@@ -28,7 +28,7 @@ export function ringCreate(texture){
 export function asteroidBelt(asteroidTexture, distance, width, count, size){
   const belt = new THREE.Group();  
   const asteroidGeo = new THREE.SphereGeometry(1, 8, 8);
-  const asteroidMat = new THREE.MeshStandardMaterial({ 
+  const asteroidMat = new THREE.MeshBasicMaterial({ 
     map: asteroidTexture,
     emissive: 0x808080
   });
