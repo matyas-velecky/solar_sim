e045f14da5ae2c6ddc575d5d03440d8d3318c9d2
matyas-velecky <matyas.velecky@gmail.com>
2026-01-14 09:06:40 +0100

    Adjust position of celestialObjects[12] in orbit

--- js/main.js
+++ js/main.js
@@ -109,7 +109,7 @@ const phobosOrbit = new THREE.Group();
 const deimosOrbit = new THREE.Group();
 celestialObjects[4].add(phobosOrbit);
 celestialObjects[4].add(deimosOrbit);
-celestialObjects[12].position.set(22,0,0);
+celestialObjects[12].position.set(24,0,0);
 celestialObjects[11].position.set(18,0,0);
 phobosOrbit.add(celestialObjects[12]);
 deimosOrbit.add(celestialObjects[11]);
